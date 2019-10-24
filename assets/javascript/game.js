e23d1988bf1bb199dff97f9e2b4d73fc3539a4fd
JACK_E <jshou403@gmail.com>
2019-10-24 16:05:17 -0400

    Reorganized and simplified JS code.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,83 @@
-var game = [ 
+var game = [
 	// question 1 
-	{q: "Where was Dale Cooper born and raised?", 
-	o: ["Missouri", "Oregon", "Indiana", "Pennsylvania"],
-	a: "Pennsylvania",
-	ff: "He was a member of the Boy Scouts and lived in Philadelphia."}, 
-    //
+	{
+		q: "Where was Dale Cooper born and raised?",
+		o: ["Missouri", "Oregon", "Indiana", "Pennsylvania"],
+		a: "Pennsylvania",
+		ff: "He was a member of the Boy Scouts and lived in Philadelphia."
+	},
+	//
 
 	// question 2 
-	{q: "What is the population of Twin Peaks?", 
-	o: ["5,021", "5,201", "51,201", "51,101"],
-	a: "51,201",
-	ff: "ABC executives were allegedly worried that TV shows in rural settings didn't attract viewers, so they added another digit onto the end so the population swelled to 51,201."}, 
+	{
+		q: "What is the population of Twin Peaks?",
+		o: ["5,021", "5,201", "51,201", "51,101"],
+		a: "51,201",
+		ff: "ABC executives were allegedly worried that TV shows in rural settings didn't attract viewers, so they added another digit onto the end so the population swelled to 51,201."
+	},
 	// https://www.vulture.com/2017/05/surprising-twin-peaks-trivia.html
 
 	// question 3 
-	{q: "What is the name of the hotel Dale stay's at in Twin Peaks?",
-	o: ["the Great Falls Lodge", "the Grand Northern Lodge", "the Great Northern Hotel", "Salish Lodge"],
-	a: "the Great Northern Hotel",
-	ff: ""}, 
+	{
+		q: "What is the name of the hotel Dale stay's at in Twin Peaks?",
+		o: ["the Great Falls Lodge", "the Grand Northern Lodge", "the Great Northern Hotel", "Salish Lodge"],
+		a: "the Great Northern Hotel",
+		ff: ""
+	},
 
 	// question 4 
-	{q: "Who does Dale speak to everyday but we never see?", 
-	o: ["Laura", "Donna", "Diane", "Bob", "Diane"],
-	ff: "It's the first word spoken by Dale in the pilot episode."}, 
+	{
+		q: "Who does Dale speak to everyday but we never see?",
+		o: ["Laura", "Donna", "Diane", "Bob", "Diane"],
+		ff: "It's the first word spoken by Dale in the pilot episode."
+	},
 
 	// question 5 
-	{q: "Who invites Icelandic business men to the Great Northern in an attempt to close a deal?", 
-	o: ["Leo and Bobby", "Jacques and Bernard", "Catherine and Ben", "Ben and Jerry"],
-	a: "Ben and Jerry"},
+	{
+		q: "Who invites Icelandic business men to the Great Northern in an attempt to close a deal?",
+		o: ["Leo and Bobby", "Jacques and Bernard", "Catherine and Ben", "Ben and Jerry"],
+		a: "Ben and Jerry"
+	},
 
 	// question 6
-	{q: "Who tells Dale that 'the owls are not what they seem'?", 
-	o: ["Leland", "the Giant", "the Old Man", "the Log Lady"],
-	a: "the Giant",
-	ff: ""}, 
+	{
+		q: "Who tells Dale that 'the owls are not what they seem'?",
+		o: ["Leland", "the Giant", "the Old Man", "the Log Lady"],
+		a: "the Giant",
+		ff: ""
+	},
 
 	// question 7
-	{q: "What is the popular TV drama everyone watches?",
-	o: ["Invitation to Love", "All Tied Up", "Lover's Lane", "Forbidden"],
-	a: "Invitation to Love",
-	ff: ""}, 
+	{
+		q: "What is the popular TV drama everyone watches?",
+		o: ["Invitation to Love", "All Tied Up", "Lover's Lane", "Forbidden"],
+		a: "Invitation to Love",
+		ff: ""
+	},
 
 	// question 8
-	{q: "What kind of bird was present at the murder of Laura Palmer?",
-	o: ["Owl", "Bewick's Wren", "Myna", "Parrot"],
-	a: "Myna",
-	ff: "Bewick's Wren (boo-icks) is the bird in the opening credits of the show. "}, 
+	{
+		q: "What kind of bird was present at the murder of Laura Palmer?",
+		o: ["Owl", "Bewick's Wren", "Myna", "Parrot"],
+		a: "Myna",
+		ff: "Bewick's Wren (boo-icks) is the bird in the opening credits of the show. "
+	},
 
 	// question 9
-	{q: "Where did Dr. Jacoby keep the other half of the heart necklace?",
-	o: ["In his spice cabinet", "In a coconut", "Under his pillow", "In his wallet"],
-	a: "In a coconut",
-	ff: ""}, 
+	{
+		q: "Where did Dr. Jacoby keep the other half of the heart necklace?",
+		o: ["In his spice cabinet", "In a coconut", "Under his pillow", "In his wallet"],
+		a: "In a coconut",
+		ff: ""
+	},
 
 	// question 10
-	{q: "Finish the quote: 'Through the darkness, future past. The magician longs to see. One chants out between two worlds. ____ ____ ____ ____.'", 
-	o: ["Answer, come to me", "Owls, help me see", "Please, help me see", "Fire, walk with me"],
-	a: "Fire, walk with me",
-	ff: ""}, 
+	{
+		q: "Finish the quote: 'Through the darkness, future past. The magician longs to see. One chants out between two worlds. ____ ____ ____ ____.'",
+		o: ["Answer, come to me", "Owls, help me see", "Please, help me see", "Fire, walk with me"],
+		a: "Fire, walk with me",
+		ff: ""
+	},
 ]
 
 // console.log("Question 8: " + game[7].q);
@@ -73,75 +93,60 @@ var winCount = 0;
 var lossCount = 0;
 var skipCount = 0;
 
+function nextQuestion() {
+
+		// Display timer
+
+		// DOESN'T WORK
+		// var displayTimeLeft = document.getElementById("timer").appendChild("div");
+		// displayTimeLeft.textContent("Time Left");
+
+		// THIS WORKS
+		// var timeCount = 30; 
+		// $("#timer").text(timeCount + " seconds");
+
+		// End Timer Code Test
+
+		// Display next question
+		var question = document.createElement("h2");
+		question.innerHTML = game[0].q;
+		document.getElementById("subject").appendChild(question);
+
+		// Display options 
+		var options = document.createElement("button");
+		options.innerHTML = game[0].o[0];
+		document.getElementById("results").appendChild(options);
+
+		var options = document.createElement("button");
+		options.innerHTML = game[0].o[1];
+		document.getElementById("results").appendChild(options);
+
+		var options = document.createElement("button");
+		options.innerHTML = game[0].o[2];
+		document.getElementById("results").appendChild(options);
+
+		var options = document.createElement("button");
+		options.innerHTML = game[0].o[3];
+		document.getElementById("results").appendChild(options);
+
+		// test and debug 
+		// console.log(question);
+		// console.log(game[0].q);
+		// console.log(game[0].o[1]);
+
+};
+
 // when the player starts the game... run the following
-$("#start-btn").on("click", function() {
-	
-	// hide A screen (class=rules, id=start-btn)
-	$("#start-btn").hide();
-	$(".rules").hide(); 
+$("#start-btn").on("click", function () {
+
+	// hide A screen (class=rules)
+	$(".rules").hide();
+
+	// run nextQuestion which will 
+	// display B screen (id=timer, id=subject, id=results)
+	nextQuestion();
 
 	// test and debug
-	console.log("start button pressed"); 
+	// console.log("start button pressed");
 
-	// display B screen (id=timer, id=subject, id=results)
-
-	// B - subect div - create div for question
-
-	// B - results div - create 4 buttons for answers
-	// https://www.w3schools.com/jsref/met_document_createelement.asp
-
-	function nextQuestion () {
-
-		// if counter is 
-
-	}
-
-	var question = document.createElement("h2");
-	question.innerHTML= game[0].q;
-	document.getElementById("subject").appendChild(question);
-
-	var options = document.createElement("button");
-	options.innerHTML = game[0].o[0];
-	document.getElementById("results").appendChild(options);
-
-	var options = document.createElement("button");
-	options.innerHTML = game[0].o[1];
-	document.getElementById("results").appendChild(options);
-
-	var options = document.createElement("button");
-	options.innerHTML = game[0].o[2];
-	document.getElementById("results").appendChild(options);
-
-	var options = document.createElement("button");
-	options.innerHTML = game[0].o[3];
-	document.getElementById("results").appendChild(options);
-
-	// B - timer
-
-	// function clockReset() {
-	// 	time = 0;
-	// 	lap = 1;
-	// 	$("#timer").text("00:30");
-	// };
-	
-	// function clockStart() {
-	// 	if (!clockRunning) {
-	// 	  intervalId = setInterval(count, -1000);
-	// 	  clockRunning = true;
-	// 	}
-	// };
-	
-	// function timerStop() {
-	// 	clearInterval(intervalId);
-	// 	clockRunning = false;
-	// };
-
-	// clockStart();
-	// document.getElementById("#subject").appendChild(clockStart);
-
-	// test and debug 
-	console.log(game[0].q);
-	console.log(question);
-	console.log(game[0].o[1]);
-
-}); 
+});
